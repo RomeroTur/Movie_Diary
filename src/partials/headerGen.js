f34d5a7977c24fffa4615fa7db3bee3993e144eb
Romero <romero.turtureanu@gmail.com>
2026-03-26 16:31:02 +0100

    test

--- src/partials/headerGen.js
+++ src/partials/headerGen.js
@@ -2,7 +2,7 @@ const headerInnerHTML = `
 <div>
 <ul>
 <li><a href="index.html">Start</a></li>
-<li><a href="journal.html">Journal</a></li>
+<li><a href="journal.html">Journal_2</a></li>
 </ul>
 </div>
 
