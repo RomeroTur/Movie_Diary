d94d9aece1a49e6248964bfd92225f1fb856d2c3
front-ente <front-ente@users.noreply.github.com>
2026-03-29 20:31:21 +0200

    add search fkt, styles

--- src/modules/api.js
+++ src/modules/api.js
@@ -7,6 +7,7 @@ const options = {
 };
 
 const dataSource = "https://api.themoviedb.org/3/movie/popular?language=de-DE&page=1";
+const dataSourceSearch = 'https://api.themoviedb.org/3/search/movie?query=';
  async function loadMovies() {
     const res = await fetch(dataSource, options);
     if (!res.ok) throw new Error("Failed to get Data");
@@ -14,4 +15,12 @@ const dataSource = "https://api.themoviedb.org/3/movie/popular?language=de-DE&pa
     return movieList.results;
 }
 
-export { loadMovies };
+async function searchMovies(query) {
+    const searchQueryUrl = `${dataSourceSearch}${encodeURIComponent(query)}&language=de-DE`
+    const res = await fetch(searchQueryUrl, options);
+    if (!res.ok) throw new Error("Failed to get Data");
+    const movieList = await res.json();
+    return movieList.results;
+}
+
+export { loadMovies, searchMovies };
--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,6 @@ import './style.css'
 // import partials & build header/footer
 import headerInnerHTML from "./partials/headerGen.js";
 import footerInnerHTML from "./partials/footerGen.js";
-import pokemonList from "./modules/fetch.js";
 
 const body = document.querySelector("body");
 const header = document.createElement("header");
@@ -19,7 +18,23 @@ body.insertAdjacentElement("beforeend", footer);
 
 // index page
 import { loadMovies } from './modules/api.js';
+import { searchMovies } from './modules/api.js';
 import { renderMovieList } from './modules/render.js';
+
+const searchform = document.querySelector('#movie-search');
+
+searchform.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const searchQuery = event.target.querySelector("input").value;
+
+    try {
+        const movies = await searchMovies(searchQuery);
+        renderMovieList(movies);
+
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 try {
     const movies = await loadMovies();
